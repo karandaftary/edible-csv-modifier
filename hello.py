3fbd191ef7e6bff6260cae4c7e2e550f9b8d364f
karanda123 <karanda@microsoft.com>
2021-11-04 14:16:58 -0700

    read first csv using panda

--- hello.py
+++ hello.py
@@ -1,19 +1,20 @@
-from time import sleep
-
-print("This is my file to demonstrate best practices.")
+import pandas as pd
 
 def process_data(data):
     print("Beginning data processing...")
     modified_data = data + " that has been modified"
-    sleep(3)
     print("Data processing finished.")
     return modified_data
 
 def main():
-    data = "My data read from the Web"
-    print(data)
-    modified_data = process_data(data)
-    print(modified_data)
+    # read csv file if it exists
+    df = pd.read_csv(r'11-04-21 Kirkland.csv', error_bad_lines=False)
+    print(df)
+    # extract fields that are necessary
+    
+    # add message
+
+    # create new csv file with extracted fields
 
 # entry point
 if __name__ == "__main__":
